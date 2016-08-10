05a2bd2453e7f8df2cf986209f9753e14d74d83f
Scott Macpherson <scottmacphersonmusic@gmail.com>
2016-08-09 19:10:51 -0700

    add UI for adding instruments

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -2,4 +2,21 @@ class InstrumentsController < ApplicationController
   def index
     @instruments = Instrument.all
   end
+
+  def new
+    @instrument = Instrument.new
+  end
+
+  def create
+    @instrument = Instrument.new(instrument_params)
+    if @instrument.save
+      redirect_to instruments_path, notice: 'Instrument Added'
+    end
+  end
+
+  private
+
+  def instrument_params
+    params.require(:instrument).permit(:name, :key, :selected)
+  end
 end
--- spec/features/instrument_spec.rb
+++ spec/features/instrument_spec.rb
@@ -19,4 +19,15 @@ describe 'Instrument' do
     expect(page).to have_content 'Flute'
     expect(page).to have_content 'Piano'
   end
+
+  it 'add an instrument' do
+    visit instruments_path
+    click_on 'New Instrument'
+    fill_in 'Name', with: 'Trumpet'
+    fill_in 'Key', with: 'Bb'
+    click_on 'Add Instrument'
+
+    expect(page).to have_content 'Instrument Added'
+    expect(page).to have_content 'Trumpet'
+  end
 end
