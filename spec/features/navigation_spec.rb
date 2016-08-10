616d6b10ea7d9ebd62da3e2a5731b48901c6578e
Scott Macpherson <scottmacphersonmusic@gmail.com>
2016-08-10 12:21:43 -0700

    add link to repertoires in nav bar

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 describe 'Navigation' do
-  it 'links to instruments in nav bar' do
+  before do
     visit root_path
+  end
 
+  it 'links to instruments in nav bar' do
     expect(find('.nav-bar')).to have_link 'Instruments'
+  end
+
+  it 'links to repertoires in nav bar' do
+    expect(find('.nav-bar')).to have_link 'Repertoires'
   end
 end
