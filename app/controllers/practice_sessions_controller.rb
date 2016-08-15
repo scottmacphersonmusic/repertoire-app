3c2a2994ae3593b69c14742dbe6a31a7941bb7f3
Scott Macpherson <scottmacphersonmusic@gmail.com>
2016-08-14 19:36:14 -0700

    no need for repertoire to be instance var

--- app/controllers/practice_sessions_controller.rb
+++ app/controllers/practice_sessions_controller.rb
@@ -1,16 +1,16 @@
 class PracticeSessionsController < ApplicationController
   def create
-    @repertoire = Repertoire.find(params[:repertoire_id])
-    @practice_session = @repertoire.practice_sessions.new
+    repertoire = Repertoire.find(params[:repertoire_id])
+    @practice_session = repertoire.practice_sessions.new
 
     if @practice_session.save
-      suggestion = @practice_session.suggestions.create!(
-        song_title: @practice_session.songs.map(&:title).sample
-      )
-      redirect_to suggestion, status: :moved_permanently
+      Suggestion.transaction do
+        suggestion = @practice_session.suggestions.create!
+        redirect_to suggestion, status: :moved_permanently
+      end
     else
       flash[:error] = 'Error Creating Practice Session'
-      redirect_to @repertoire, status: :moved_permanently
+      redirect_to repertoire, status: :moved_permanently
     end
   end
 end
